fc22635c9d09bea8e92cd44d9e3645ca4479d6c0
bruno <bruno.lameirinha@gmail.com>
2024-08-14 00:24:45 -0300

    testing envp

--- src/minishell.c
+++ src/minishell.c
@@ -1,17 +1,31 @@
 # include "../include/minishell.h"
 
-int main() {
-    char *input;
-    int i = 0;
-    while(i<3)
-    {
-        input = readline("$");
-        if (input) 
-        {
-            printf("Você digitou: %s\n", input);
-            free(input);
-            i++;
-        }
+int main(int argc, char *argv[], char *envp[])
+{
+	
+	char *input;
+	int i;
+
+	if (argc == 1 && argv[0] != NULL)
+	{
+	i = 0;
+
+	while(i < 3)
+	{
+		input = readline("$");
+		if (input) 
+		{
+			printf("Você digitou: %s\n", input);
+			free(input);
+			i++;
+		}
+	}
+
+    while (envp[i] != NULL) {
+		if(!strncmp((envp[i]), "OLDPWD", 6))
+        	printf("%s\n", envp[i]);
+        i++;
     }
-    return(0);
+	}
+    return 0;
 }
