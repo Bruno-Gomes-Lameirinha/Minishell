2fdcf8b719f73256959fbc6f887e2ef4671aeaf0
Bruno Gomes Lameirinha <bgomes-l@c1r4p2.42sp.org.br>
2024-08-15 14:28:29 -0300

    included supressed file

--- src/minishell.c
+++ src/minishell.c
@@ -3,34 +3,21 @@
 int main()
 {
 	char *input;
-	int i;
 	t_env	*env;
+	int i = 0;
 
 	env = (t_env*)malloc(sizeof(t_env));
-	i = 0;
-	env->pwd = getenv("PWD");
-	printf("pwd: %s\n", env->pwd);
-	while(i < 3)
+	while(i <3)
 	{
-		input = readline("$");
+		input = readline("Minishell$ ");
 		add_history(input);
 		if (input) 
 		{
 			printf("Você digitou: %s\n", input);
-			if(strncmp(input, "cd", 2))
-			{
-				env->old_pwd = getenv("PWD");
-				if (chdir("/home/bruno/42sp/minishell/include") == 0)
-					env->pwd = getcwd(NULL, 0);
-			}
-			else
-				perror("chdir");
 			free(input);
 			i++;
 		}
 	}
-	env->pwd = getcwd(NULL, 0);
-	printf("pwd: %s\n", env->pwd); 
-	
-    return 0;
+	free(env);
+	return 0;
 }
