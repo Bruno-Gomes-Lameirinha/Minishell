0324bb325d5795436fd177dc985c90e3498a4e34
bruno <bruno.lameirinha@gmail.com>
2024-08-26 23:55:26 -0300

    cd fix

--- src/built_in.c
+++ src/built_in.c
@@ -38,12 +38,15 @@ void	ft_cd_command(t_token **cmd_tokens)
 	char	*pwd;
 
 	current_token = *cmd_tokens;
-	if (current_token->next == NULL)
+	if (current_token->next == NULL && !strcmp(current_token->token_node, "cd"))
 		path = getenv("HOME");
-	else
+	else if (current_token->next)
 		path = current_token->next->token_node;
 	if (chdir(path) != 0)
+	{
 		perror("cd");
+		return;
+	}
 	pwd  = getcwd(NULL, 1024);
 	printf("%s\n", pwd);
 	free(pwd);
